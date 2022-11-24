7d065920e6d3c80eb43c48b0b1ceb035e1fad211
rohanaswani41 <rohanaswani41@gmail.com>
2022-11-23 21:06:47 -0500

    added defence

--- SQLInject/app/app.py
+++ SQLInject/app/app.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask,request, render_template
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 from serializers import AlchemyEncoder
 
@@ -66,6 +67,23 @@ def secure():
                 }
             )
         return json.dumps(ret)
+    
+@app.route('/secure-nonorm-profule-details',methods=['GET','POST'])
+def secure_nonorm():
+
+    if request.method == 'POST':
+        user = request.form['username']
+        stmt = text("select * from user where username = :s")
+        res = connection.execute(stmt, s=user)
+        output = []
+        for obj in res:
+            single = {}
+            single['id'] = obj[0]
+            single['username'] = obj[1]
+            single['email'] = obj[2]
+            output.append(single)
+        return json.dumps(output)
+
 
 
 
